c293c86eb029a045792faaa262017a276b9016b0
M1sh44l <mishaal@outlook.com>
2017-07-25 18:47:03 +0300

    Create, Update, Delete and some changes

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Post
 from django.shortcuts import get_object_or_404
 from .forms import PostForm
+from django.contrib import messages
 
 # Create your views here.
 
@@ -34,13 +35,33 @@ def post_detail(request, post_id):
 	return render(request, "post_detail.html", context)
 
 def post_create(request):
-	form = PostForm()
+	form = PostForm(request.POST or None)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "OMG! Nice one!")
+		return redirect("posts:list")
 	context = {
 		"form": form,
 	}
 	return render(request, "post_create.html", context)
 
+def post_update(request, post_id):
+	post_object = get_object_or_404(Post, id=post_id)
+	form = PostForm(request.POST or None, instance=post_object)
+	if form.is_valid():
+		form.save()
+		messages.success(request, "Any second thought?")
+		return redirect("posts:list")
+	context = {
+		"form": form,
+		"post_object": post_object,
+	}
+	return render(request, "post_update.html", context)
 
-
-
+def post_delete(request, post_id):
+	delete_object = Post.objects.get(id=post_id)
+	delete_object.delete()
+	#or the above could be done in one line: delete_object = = Post.objects.get(id=post_id).delete()
+	messages.warning(request, "Seriously bro?")
+	return redirect("posts:list")
 
